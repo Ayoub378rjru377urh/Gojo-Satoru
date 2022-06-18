a4ba8f10b2f8c0c27b5ca019228faef474a3e759
Yukibot52 <107733656+Yukibot52@users.noreply.github.com>
2022-06-19 01:26:28 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['201016651321']
+global.premium = ['201016651321']
+global.ownernomer = '201016651321'
+global.ownername = 'اكيرا ❤🖇'
+global.botname = 'Yuki Bot'
+global.footer = 'اكيرا ❤🖇'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'Yuki-Bot'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
